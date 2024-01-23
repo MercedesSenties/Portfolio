d786eaeb99c67250c808abdedea54974326d8f70
MercedesSenties <anamercedes.senties@gmail.com>
2024-01-23 15:29:47 -0600

    style: added items center

--- components/reusable/Carousel.jsx
+++ components/reusable/Carousel.jsx
@@ -4,13 +4,11 @@ import Image from "next/image";
 const ImageCarousel = ({ images, width, height }) => {
   return (
     <div>
-      <Carousel>
+      <Carousel className="items-center">
         {images.map((item, i) => {
           if (item.type === "image") {
             return (
-              <div
-                className="duration-700 ease-in-out"
-                key={i}>
+              <div className="duration-700 ease-in-out" key={i}>
                 <Image
                   src={item.src}
                   className="block"
@@ -22,13 +20,12 @@ const ImageCarousel = ({ images, width, height }) => {
             );
           } else if (item.type === "video") {
             return (
-              <div
-                className="duration-700 ease-in-out"
-                key={i}>
+              <div className="duration-700 ease-in-out" key={i}>
                 <iframe
                   className="w-full aspect-video"
                   src={`https://www.youtube-nocookie.com/embed/${item.src}`}
-                  title="YouTube video player"></iframe>
+                  title="YouTube video player"
+                ></iframe>
               </div>
             );
           }
